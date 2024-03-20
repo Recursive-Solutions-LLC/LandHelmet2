ea1a501d4194647bb20bb4128bb3bc2d6bdbd084
Drakenwolf <hans.haar6@gmail.com>
2024-03-20 16:52:10 -0500

    modify colors

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import bannerBg from '../assets/img/page-banner.jpg';
-import Footer1 from '../components/Footer1';
-import Header1 from '../components/Header1';
+import Footer3 from '../components/Footer3';
+import Header3 from '../components/Header3';
 import PageBanner from '../components/PageBanner';
 import Process from '../components/Process';
 import ProjectFilter2 from '../components/ProjectFilter2';
@@ -10,12 +10,12 @@ import Services2 from '../components/Services2';
 function Services() {
     return (
         <>
-            <Header1 />
+            <Header3 />
             <PageBanner bannerBg={bannerBg} currentPage="Services" heading="What We Do" />
             <Services2 />
             <Process />
             <ProjectFilter2 />
-            <Footer1 />
+            <Footer3 />
         </>
     );
 }
