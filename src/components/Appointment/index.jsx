3f100879aadac6e384bcee9ce1c6550e3924d1ad
Skillet-95 <yfeijoo95@gmail.com>
2024-03-21 20:28:07 -0400

    updarte map

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -39,7 +39,7 @@ function Apointment() {
                         >
                             <iframe
                                 title="map"
-                                src="https://maps.app.goo.gl/UPajkHEcYwouuARi7"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3314.7974090731714!2d-118.02898198844616!3d33.8175408299748!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd2f55a1453e31%3A0x5071f95e3a357f2d!2sLand%20Helmets%20%2F%20GTC%20Land%20Sports%20INC!5e0!3m2!1ses-419!2sus!4v1711026326727!5m2!1ses-419!2sus"
                                 style={{ border: '0', height: '100%', width: '100%' }}
                                 loading="lazy"
                             />
