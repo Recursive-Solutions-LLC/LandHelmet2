8adce90a5a94a419716a813a1659d91dda30cc2e
Skillet-95 <yfeijoo95@gmail.com>
2024-03-23 14:01:17 -0400

    new images Advanced Helmet Solutions

--- src/components/Portfolio2/porfolioTwoData.js
+++ src/components/Portfolio2/porfolioTwoData.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
-import img1 from '../../assets/img/portfolio/cases1.jpg';
-import img2 from '../../assets/img/portfolio/cases2.jpg';
+import img1 from '../../assets/img/portfolio/1.png';
+import img2 from '../../assets/img/portfolio/2.png';
 
 
 const portfolioTwoData = [
