55beb6be1dd93bdde5db9379e4d69a2c8db5f1ce
Drakenwolf <hans.haar6@gmail.com>
2024-03-24 17:29:22 -0500

    fix footer

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import bannerBg from '../assets/img/page-banner.jpg';
 import Footer1 from '../components/Footer1';
 import Header1 from '../components/Header3';
-import PageBanner from '../components/PageBanner';
 import PortfolioDetails from '../components/PortfolioDetails';
+import Hero1 from '../components/Hero1';
 
 function ProjectDetails(data) {
     return (
         <>
             <Header1 />
-            <PageBanner
-                bannerBg={bannerBg}
-                heading="Product Details"
-                currentPage="Product Details"
-            />
+            <Hero1 />I
             <PortfolioDetails type={data} />
             <Footer1 />
         </>
