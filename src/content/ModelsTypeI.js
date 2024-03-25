e6e1c6ace2a655532984a8025cbfbd92a8c2cfe8
Drakenwolf <hans.haar6@gmail.com>
2024-03-25 16:57:34 -0500

    change products

--- src/content/ModelsTypeI.js
+++ src/content/ModelsTypeI.js
@@ -1,9 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import Bg1 from '../assets/img/home1/eng.jpg';
-import Bg4 from '../assets/img/home1/gas.jpg';
 import Bg2 from '../assets/img/home1/power.jpg';
 import typeI from '../assets/img/icon/helmet-types/typeI.svg';
-import typeII from '../assets/img/icon/helmet-types/typeII.svg';
 
 
 
@@ -22,18 +20,6 @@ const ModelsTypeI = [
         heading: 'M02',
         btnText: 'Contact Us',
     },
-
-
-
-    {
-        id: uuidv4(),
-        bgImg: Bg4,
-        icon: typeII,
-        heading: 'M10',
-        btnText: 'Contact Us',
-    },
-
-
 ];
 
 export default ModelsTypeI;
---
+++ src/content/ModelsTypeII.js
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from 'uuid';
+import Bg4 from '../assets/img/home1/gas.jpg';
+import typeII from '../assets/img/icon/helmet-types/typeII.svg';
+
+
+
+const ModelsTypeI = [
+
+    {
+        id: uuidv4(),
+        bgImg: Bg4,
+        icon: typeII,
+        heading: 'M10',
+        btnText: 'Contact Us',
+    },
+
+
+];
+
+export default ModelsTypeI;
