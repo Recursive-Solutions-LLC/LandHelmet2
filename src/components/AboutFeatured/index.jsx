99bf5ae11c01c494d3937eb58cbf6df6e20ca04c
Drakenwolf <hans.haar6@gmail.com>
2024-03-27 12:48:07 -0500

    update cover of about us, udpate about sections and replace images on our approach

--- src/components/AboutFeatured/index.jsx
+++ src/components/AboutFeatured/index.jsx
@@ -1,74 +1,39 @@
 import React from 'react';
 import '../../../node_modules/react-modal-video/scss/modal-video.scss';
 import aboutImg from '../../assets/img/about_us.jpg';
-import IconBox from './IconBox';
-import TypeI from "../../assets/img/icon/helmet-types/typeI.svg"
-import TypeII from "../../assets/img/icon/helmet-types/typeI.svg"
 
-function AboutFeatured() {
+function AboutFeatured({ aboutContent }) {
+    const { title, subtitle, text, right } = aboutContent;
 
-
-    const renderIcon = (src, alt) =>{
-        return(
-            <img src={src} alt={alt} />
-        )
-    }
-
-    const renderTypeI= renderIcon(TypeI,"helmet type I");
-    const renderTypeII= renderIcon(TypeII,"helmet type II");
-
-    const title = "Located in Orange County, California."
-    const subtitle = "Proudly operating under the sponsorship of Aurora Sports"
-    const subtitle2 = "Welcome to Land Helmets, your premier destination for premium Safety Helmets Type I and Type II (SEI Certified)."
-
-    const helmet = [
-        {
-            heading: "Unmatched Safety Standards:",
-            text: "Our Type I helmets are ANSI Z89.1-2014 (R2019) certified, ensuring they meet the highest safety requirements for impact protection in the industry."
-        },
-        {
-            heading: "Certifications for Confidence:",
-            text: "Certified to meet ANSI Z89.1-2014 (R2019) standards, our Type II helmet ensures compliance with stringent safety requirements."
-        }
-    ]
+    // Determine the order of the columns based on the 'right' property
+    const imageColumnClass = right ? "order-lg-2" : "";
+    const textColumnClass = right ? "order-lg-1" : "";
 
     return (
+        <section className="about-featured-wrapper pt-5 mt-5">
+            <div className="container">
+                <div className="row align-items-center">
+                    {/* Adjust the order of this column based on 'right' */}
+                    <div className={`col-xl-6 col-lg-5 col-12 ${imageColumnClass}`}>
+                        <div
+                            className="about-promo bg-cover"
+                            style={{ backgroundImage: `url(${aboutImg})` }}
+                        />
+                    </div>
 
-            <section className="about-featured-wrapper section-padding">
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-xl-6 col-lg-5 col-12">
-                            <div
-                                className="about-promo bg-cover"
-                                style={{ backgroundImage: `url(${aboutImg})` }}
-                            />
-                        </div>
-
-                        <div className="col-xl-6 col-lg-7 col-12 mt-5 mt-lg-0">
-                            <div className="block-contents ml-lg-5">
-                                <span>{subtitle}</span>
-                                <h1>{title}</h1>
-                                <h4>
-                                   {subtitle2}
-                                </h4>
-                            </div>
-
-                            <div className="icon-boxs ml-lg-5">
-                                <IconBox
-                                    icon={renderTypeI }
-                                    heading={helmet[0].heading}
-                                    text={helmet[0].text}
-                                />
-                                <IconBox
-                                    icon={renderTypeII} 
-                                    heading={helmet[1].heading}
-                                    text={helmet[1].text}
-                                />
-                            </div>
+                    {/* Adjust the order of this column based on 'right' */}
+                    <div className={`col-xl-6 col-lg-7 col-12 mt-5 mt-lg-0 ${textColumnClass}`}>
+                        <div className="block-contents ml-lg-5">
+                            <span>{subtitle}</span>
+                            <h1>{title}</h1>
+                            <h4>
+                                {text}
+                            </h4>
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
     );
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,13 +9,39 @@ import Approch from '../components/Approch';
 import Timeline from '../components/Timeline';
 import Cta3 from '../components/Cta2';
 
+const about1 ={
+    right: false,
+    title: "Located in Orange County, California.",
+    subtitle: "Welcome to GTC LAND SPORTS INC",
+    text: "Based in the vibrant community of Cypress, CA, GTC LAND SPORTS INC is a dynamic startup company driven by professionalism, agility, and unwavering customer focus. Our mission is simple yet ambitious: to turn opportunities into tangible realities through a blend of research, evaluation, innovation, and design."
+}
+
+const about2 ={
+    right: true,
+    title: "At GTC LAND SPORTS INC",
+    subtitle: "Our Vision",
+    text: "we believe in the power of transformation. By harnessing the principles of impactful branding, user-centric product design, and practical engineering, we strive to create products that not only meet but exceed market expectations."
+}
+
+const about3 ={
+    right: false,
+    title: "The heart of our company lies in our name",
+    subtitle: "Our Philosophy",
+    text: ' GTC LAND. Here, "LAND" stands as our registered trademark, while each letter represents our core values: "G" for Gospelized, "T" for Truthized, and "C" for Churchized. Our ultimate goal is to supply spiritual helmets by providing material helmets, aligning our purpose with the divine mission.'
+}
+
+
 function About() {
     return (
         <>
             <Header3 />
      
             <PageBanner bannerBg={CoverAbout} currentPage="About Us" heading="About Us" />
-            <AboutFeatured />
+            <section className='section-padding'>
+                <AboutFeatured aboutContent={about1}/>
+                <AboutFeatured aboutContent={about2}/>
+                <AboutFeatured aboutContent={about3}/>
+            </section>
             <Approch />
             <Timeline/>
             <About2/>
