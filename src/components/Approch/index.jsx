f9ff439011ee1548415a2b676a6f5e9fe567d126
Drakenwolf <hans.haar6@gmail.com>
2024-04-10 17:24:15 -0500

    change text in about us, remove aproach component

--- src/components/Approch/index.jsx
+++ src/components/Approch/index.jsx
@@ -1,33 +1,23 @@
 import React from 'react';
 import ApprochCard from './ApprochCard';
-import approchData from './approchData';
-import approchDataHome from './approchDataHome';
 
-const renderAproachCards = approchData.map((data) => (
-    <ApprochCard
-        key={data.id}
-        thumbnail={data.thumb}
-        icon={data.icon}
-        heading={data.heading}
-        text={data.text}
-        btnText={data.btnText}
-        pageLink={data.pageLink}
-    />
-))
-const renderAproachCards2 = approchDataHome.map((data) => (
-    <ApprochCard
-        key={data.id}
-        thumbnail={data.thumb}
-    
-        heading={data.heading}
-        text={data.text}
-        btnText={data.btnText}
-        pageLink={data.pageLink}
-    />
-))
+
+
 function Approch(data) {
+
+    const renderAproachCards = data.map((item) => (
+        <ApprochCard
+            key={item.id}
+            thumbnail={item.thumb}
+            icon={item.icon}
+            heading={item.heading}
+            text={item.text}
+            btnText={item.btnText}
+            pageLink={item.pageLink}
+        />
+    ))
     return (  <section className="our-approch-wrapper section-padding section-bg">
-  {data.type===1 && <div className="container">
+  <div className="container">
         <div className="row mb-30">
             <div className="col-12 col-lg-12">
                 <div className="section-title text-center">
@@ -39,21 +29,7 @@ function Approch(data) {
         <div className="row">
             {renderAproachCards}
         </div>
-    </div>}  
-    {data.type===2 && <div className="container">
-        <div className="row mb-30">
-            <div className="col-12 col-lg-12">
-                <div className="section-title text-center">
-                    <h1>Unlock The Full Potential Of Your Product</h1>
-                    <h2> and Bring Your Vision To Life with US</h2>
-                </div>
-            </div>
-        </div>
-
-        <div className="row">
-            {renderAproachCards2}
-        </div>
-    </div>}  
+    </div>
 </section>
      
     );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Footer3 from '../components/Footer1';
 import Header3 from '../components/Header3';
 import About2 from '../components/About2';
- import CoverAbout from '../assets/img/about/cover.jpg';
- import PageBanner from '../components/PageBanner';
- import AboutFeatured from '../components/AboutFeatured';
+import CoverAbout from '../assets/img/about/cover.jpg';
+import PageBanner from '../components/PageBanner';
+import AboutFeatured from '../components/AboutFeatured';
 import Approch from '../components/Approch';
 import Timeline from '../components/Timeline';
 import Cta3 from '../components/Cta2';
@@ -13,29 +13,53 @@ import about_1 from "../assets/img/about/about-1.jpg";
 import about_2 from "../assets/img/about/about-2.png";
 
 import about_3 from "../assets/img/about/about-3.png";
+import about_4 from "../assets/img/about/about-4.png";
 
-const about1 ={
-    aboutImg: about_1 ,
+const about1 = {
+    aboutImg: about_1,
     right: false,
     title: "Located in Orange County, California.",
-    subtitle: "Welcome to GTC LAND SPORTS INC",
-    texts:[ "Based in the vibrant community of Cypress, CA, GTC LAND SPORTS INC is a dynamic startup company driven by professionalism, agility, and unwavering customer focus. Our mission is simple yet ambitious: to turn opportunities into tangible realities through a blend of research, evaluation, innovation, and design."]
+    subtitle: "Welcome to Land Helmets",
+    texts: ["With a combined 28+ years of experience, our company has the working knowledge and network of resources to meet the ever changing demand of today's maket.",
+        "Our headquarter is based in Cypress, CA USA where main operations, sales, and design teams are located and our factory and production team in China.",
+        "Our Asia staff allows us to manage customer defined QC measures by being present on-site during production and at the end of final inspection before shipment."]
 }
 
-const about2 ={
+const about2 = {
     aboutImg: about_2,
     right: true,
-    title: "At GTC LAND SPORTS INC",
-    subtitle: "Our Vision",
-    texts:[ "we believe in the power of transformation. By harnessing the principles of impactful branding, user-centric product design, and practical engineering, we strive to create products that not only meet but exceed market expectations."]
+    title: "At Land Helmets",
+    subtitle: "Our mission statement:",
+    texts: ["Manufacturing top quality products, through strict QC (quality control) measures and cost-efficient supply channels, is the highest priority at Helmet Solutions",
+        "In today's highly competitive business world, Helmet Solutions realize quality products separate successful businesses from its competition. Therefore, we strive to consistently manufacture top quality products by two means:"
+    ],
+    bullets: [
+        "Implementing strict QC measures by using in-house staff to manage the supply chaines at all ends of the floor",
+        "Using certified factories in China to ensure cost savings is to our customers' advantage."
+    ]
+
 }
 
-const about3 ={
+const about3 = {
     aboutImg: about_3,
     right: false,
     title: "The heart of our company lies in our name",
     subtitle: "Our Philosophy",
-    texts:[ ' GTC LAND. Here, "LAND" stands as our registered trademark, while each letter represents our core values: "G" for Gospelized, "T" for Truthized, and "C" for Churchized. Our ultimate goal is to supply spiritual helmets by providing material helmets, aligning our purpose with the divine mission.']
+    texts: [' GTC LAND. Here, "LAND" stands as our registered trademark, while each letter represents our core values: "G" for Gospelized, "T" for Truthized, and "C" for Churchized. Our ultimate goal is to supply spiritual helmets by providing material helmets, aligning our purpose with the divine mission.']
+}
+
+
+const about4 = {
+    aboutImg: about_4,
+    right: true,
+    title: "Industry best services at highly factory direct competitive pricing",
+    subtitle: "",
+    texts: [
+        "Total in-house control from design to production to delivery.",
+        "Highly knowledgeable and experienced personnel with confidentiality controls in place.",
+        "Dedicated focus from an account manager based in the United States who is assigned to every client and project from start to finish."
+    ]
+
 }
 
 
@@ -43,17 +67,18 @@ function About() {
     return (
         <>
             <Header3 />
-     
+
             <PageBanner bannerBg={CoverAbout} currentPage="About Us" heading="About Us" />
             <section className='section-padding'>
-                <AboutFeatured aboutContent={about1}/>
-                <AboutFeatured aboutContent={about2}/>
-                <AboutFeatured aboutContent={about3}/>
+                <AboutFeatured aboutContent={about1} />
+                <AboutFeatured aboutContent={about2} />
+                <AboutFeatured aboutContent={about3} />
+
+                <AboutFeatured aboutContent={about4} />
+
             </section>
-            <Approch type={1}/>
-            <Timeline/>
-            <About2/>
-            <Cta3/>
+            <About2 />
+            <Cta3 />
             <Footer3 />
         </>
     );
