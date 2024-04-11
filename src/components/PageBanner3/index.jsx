9d35780c82e998d44a4bb48cec55e1c302b71120
Skillet-95 <yfeijoo95@gmail.com>
2024-04-11 15:06:45 -0500

    add tag

--- src/components/PageBanner3/index.jsx
+++ src/components/PageBanner3/index.jsx
@@ -6,7 +6,7 @@ function PageBanner2({ bannerBg }) {
 
 
     return (
-        <>  
+        <>
 
 
             <div className="pt-5">
@@ -14,9 +14,9 @@ function PageBanner2({ bannerBg }) {
                     <div className="col-lg-12 col-12 text-center">
                         <div className="section-title-5 text-center center-content">
 
-                         
-                            <h2>You can print your custom LOGO on pre-selected areas 
-of our Safety Helmets.</h2>
+
+                            <h2>You can print your custom LOGO on pre-selected areas
+                                of our Safety Helmets.</h2>
                         </div>
                     </div>
                 </div>
@@ -25,20 +25,39 @@ of our Safety Helmets.</h2>
                 <div className="row">
                     <div className="col-lg-12 col-12 text-center">
                         <div className="section-title-2  text-center center-content">
-                      
-                            <div className='row logos-padding  '>
+                            <div className='container'>
+                                <div className='row  '>
 
-                  
-                                <div className='col-6 mt-5 '>
-                              
-                                    <img src={img1} alt='img1' className='border' />
-                                </div>
 
-                                <div className='col-6 mt-5 '> <img src={img2} className='border' alt='img1' /></div>
-                                <div className='col-6 col-md-offset-3 mt-5 '><img src={img3} className='border' alt='img1' /></div>
-                                
+                                    <div className='col-6 mt-5'>
+                                        <div style={{ position: 'relative', display: 'inline-block' }}>
+                                            <img src={img1} alt='img1' className='border' style={{ width: '100%', display: 'block' }} />
+                                            <h1 style={{ position: 'absolute', top: '10%', left: '10%', transform: 'translate(-50%, -50%)', color: 'white', background: "#73bc25", padding: '2px 8px' }}>
+                                                M02
+                                            </h1>
+                                        </div>
+                                    </div>
+                                    <div className='col-6 mt-5'>
+                                        <div style={{ position: 'relative', display: 'inline-block' }}>
+                                            <img src={img2} alt='img1' className='border' style={{ width: '100%', display: 'block' }} />
+                                            <h1 style={{ position: 'absolute', top: '10%', left: '10%', transform: 'translate(-50%, -50%)', color: 'white', background: "#73bc25", padding: '2px 8px'  }}>
+                                                M12
+                                            </h1>
+                                        </div>
+                                    </div>
+                                    <div className='col-6 col-md-offset-3  mt-5'>
+                                        <div style={{ position: 'relative', display: 'inline-block' }}>
+                                            <img src={img3} alt='img1' className='border' style={{ width: '100%', display: 'block' }} />
+                                            <h1 style={{ position: 'absolute', top: '10%', left: '10%', transform: 'translate(-50%, -50%)', color: 'white', background: "#73bc25", padding: '2px 8px' }}>
+                                                M10
+                                            </h1>
+                                        </div>
                                     </div>
                                   
+
+
+                                </div>
+                            </div>
                         </div>
 
 
