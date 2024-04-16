cb725560f78f52d1ab50cd32e30631a92de0b488
Skillet-95 <yfeijoo95@gmail.com>
2024-04-16 18:41:00 -0500

    fix padding

--- src/components/Services3/index.jsx
+++ src/components/Services3/index.jsx
@@ -5,7 +5,7 @@ import servicesThreeData from './servicesThreeData';
 function Services3() {
     return (
         <section className="services-wrapper services-2 ">
-            <div className="section-title-5 text-center center-content">
+            <div className="section-title-5 text-center center-content pt-4 pb-4">
 
                 <h1 className="">Your Best Helmet Solution Provider </h1>
                 <p className='pb-4 pt-0'>We're thrilled to present our unparalleled ODM & OEM capabilities tailored specifically for safety helmets. As a pioneering force in the industry, we understand the critical importance of superior quality, innovative design, and uncompromising safety standards</p>
