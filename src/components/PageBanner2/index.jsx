7b6535fb073a1642aaf4cdd7e1bf13c2d6f14240
Drakenwolf <hans.haar6@gmail.com>
2024-04-17 13:13:57 -0500

    fix spacing

--- src/components/PageBanner2/index.jsx
+++ src/components/PageBanner2/index.jsx
@@ -17,13 +17,15 @@ function PageBanner2({ bannerBg }) {
 
             <div className="">
                 <div className="row">
-                    <div className="col-lg-12 col-12 text-center">
-                        <div className="section-title-5 text-center center-content">
+                    <section className="section-padding">
+                        <div className="col-lg-12 col-12 text-center">
+                            <div className="section-title-5 text-center center-content">
 
-                            <h1 className='pb-0'>Empower Your Brand</h1>
-                            <p className='pt-0 pb-4'>Personalized Safety Helmets Capabilities where safety meets style and branding excellence, tailored specifically for your business needs!</p>
+                                <h1 className='pb-4 pt-4'>Empower Your Brand</h1>
+                                <p className='pt-4 pb-4'>Personalized Safety Helmets Capabilities where safety meets style and branding excellence, tailored specifically for your business needs!</p>
+                            </div>
                         </div>
-                    </div>
+                    </section>
                 </div>
             </div>
             <div className="">
@@ -41,7 +43,7 @@ function PageBanner2({ bannerBg }) {
 
                                     <div className='col-4'> <img src={img2} alt='img1' /></div>
                                     <div className='col-4'><img src={img3} alt='img1' /></div>
-                                   
+
                                 </div>
                                 <p className=''>Land Helmets exclusively offers the option to add your logo. We do not engage in co-branding. </p>
 
